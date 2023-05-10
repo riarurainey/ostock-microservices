be6df9c5c3a33e1538c5de6c2d0788e9d40eea25
riarurainey <riarurainey@yandex.ru>
2023-05-10 19:43:02 +0300

    Added: Feign, Discovery, Rest Clients

--- README.md
+++
@@ -1,7 +0,0 @@
-VAULT
-docker run --cap-add=IPC_LOCK -e 'VAULT_DEV_ROOT_TOKEN_ID=myroot' -e 'VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200' -p 8200:8200 --name=dev-vault vault
-curl -X "GET" "http://localhost:8071/licensing-service/default" -H "X-Config-Token: myroot"
-
-Docker
-$ mvn clean package dockerfile:build
-$ docker-compose -f docker/docker-compose.yml up
--- licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -1,44 +1,79 @@
 package com.optimagrowth.license.service;
 
-import java.util.List;
-import java.util.UUID;
-
+import com.optimagrowth.license.config.ServiceConfig;
+import com.optimagrowth.license.model.License;
+import com.optimagrowth.license.model.Organization;
+import com.optimagrowth.license.repository.LicenseRepository;
+import com.optimagrowth.license.service.client.OrganizationDiscoveryClient;
+import com.optimagrowth.license.service.client.OrganizationFeignClient;
+import com.optimagrowth.license.service.client.OrganizationRestTemplateClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
-import com.optimagrowth.license.config.ServiceConfig;
-import com.optimagrowth.license.model.License;
-import com.optimagrowth.license.repository.LicenseRepository;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class LicenseService {
 
-    private final MessageSource messages;
+    private final MessageSource messageSource;
     private final LicenseRepository repository;
     private final ServiceConfig config;
+    private final OrganizationDiscoveryClient organizationDiscoveryClient;
+    private final OrganizationFeignClient organizationFeignClient;
+    private final OrganizationRestTemplateClient organizationRestTemplateClient;
 
-
-    public License getLicense(String licenseId, String organizationId) {
+    public License getLicense(String licenseId, String organizationId, String clientType) {
         License license = repository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
         if (null == license) {
-            throw new IllegalArgumentException(
-                    String.format(messages.getMessage("license.search.error.message", null, null),
-                            licenseId, organizationId));
+            throw new IllegalArgumentException(String.format(messageSource.getMessage("license.search.error.message", null, null), licenseId, organizationId));
         }
+
+        Organization organization = retrieveOrganizationInfo(organizationId, clientType);
+        if (null != organization) {
+            license.setOrganizationName(organization.getName());
+            license.setContactName(organization.getContactName());
+            license.setContactEmail(organization.getContactEmail());
+            license.setContactPhone(organization.getContactPhone());
+        }
+
         return license.withComment(config.getProperty());
+    }
+
+    private Organization retrieveOrganizationInfo(String organizationId, String clientType) {
+        Organization organization = null;
+
+        switch (clientType) {
+            case "feign" -> {
+                System.out.println("I am using the feign client");
+                organization = organizationFeignClient.getOrganization(organizationId);
+            }
+            case "rest" -> {
+                System.out.println("I am using the rest client");
+                organization = organizationRestTemplateClient.getOrganization(organizationId);
+            }
+            case "discovery" -> {
+                System.out.println("I am using the discovery client");
+                organization = organizationDiscoveryClient.getOrganization(organizationId);
+            }
+            default -> organization = organizationRestTemplateClient.getOrganization(organizationId);
+        }
+
+        return organization;
     }
 
     public License createLicense(License license) {
         license.setLicenseId(UUID.randomUUID().toString());
         repository.save(license);
+
         return license.withComment(config.getProperty());
     }
 
     public License updateLicense(License license) {
         repository.save(license);
+
         return license.withComment(config.getProperty());
     }
 
@@ -47,9 +82,13 @@ public class LicenseService {
         License license = new License();
         license.setLicenseId(licenseId);
         repository.delete(license);
-        responseMessage = String.format(messages.getMessage("license.delete.message", null, null), licenseId);
+        responseMessage = String.format(messageSource.getMessage("license.delete.message", null, null), licenseId);
         return responseMessage;
 
+    }
+
+    public List<License> getLicensesByOrganization(String organizationId) {
+        return repository.findByOrganizationId(organizationId);
     }
 
 }
